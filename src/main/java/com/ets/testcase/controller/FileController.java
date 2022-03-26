d3df358b39ed05a20b44088a142bf0a538105a56
yhbaran@gmail.com <19Yhbara68!>
2022-03-27 02:26:11 +0300

    added delete method for FileController

--- src/main/java/com/ets/testcase/controller/FileController.java
+++ src/main/java/com/ets/testcase/controller/FileController.java
@@ -8,6 +8,7 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -82,6 +83,11 @@ public class FileController {
                 .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"" + fileEntity.getName() + "\"")
                 .contentType(MediaType.valueOf(fileEntity.getContentType()))
                 .body(fileEntity.getData());
+    }
+
+    @DeleteMapping("{id}")
+    public void deleteFile(@PathVariable Long id){
+        fileService.deleteFile(id);
     }
 
 }
