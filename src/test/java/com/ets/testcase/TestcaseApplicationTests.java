49224efa75f3228db186c4ebbc449c8054e146df
yhbaran@gmail.com <19Yhbara68!>
2022-03-26 11:29:05 +0300

    initial commit

---
+++ src/test/java/com/ets/testcase/TestcaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ets.testcase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestcaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
